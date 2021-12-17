647ff9459108486cd1405bd2700a6f4e8ceb07c4
GPGRedman <b8025183@ncl.ac.uk>
2021-12-17 17:33:35 +0000

    final version

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,16 +6,6 @@
 sleep 15
 # Apply database migrations
 echo "Apply database migrations"
-echo $PWD
-#this mv is a hack. I could not get git to put the code in the right place
-echo "***"
-ls /code
-echo "***"
-ls /code/code
-echo "***"
-ls /code/code/SDCode
-echo "***"
-
 python manage.py migrate
 
 # create a superuser
@@ -27,5 +17,4 @@ python manage.py createsuperuser --noinput
 
 # Start server
 echo "Starting server"
-
-# python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
